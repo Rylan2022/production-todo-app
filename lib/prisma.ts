ce0fd0c05b7536ba94b60256f642fdd2044cec58
Rakesh Molla <rakesh2024molla@yahoo.com>
2026-08-02 22:47:41 +0530

    done migration

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+
+const adapter = new PrismaLibSql({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
